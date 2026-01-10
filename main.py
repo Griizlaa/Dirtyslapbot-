ed477795792ee1b25cc7ba1097af1aadc2c5078a
Griizlaa <93580226+Griizlaa@users.noreply.github.com>
2026-01-10 03:38:45 +0200

    Fix MAIN_POST_ID environment variable retrieval

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ access_token = os.getenv('ACCESS_TOKEN')
 access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
 
 # Your pinned post ID
-    MAIN_POST_ID = os.getenv(key)  # Railway variable
+MAIN_POST_ID = os.getenv('MAIN_POST_ID')  # correct – string in quotes!
 
 client = tweepy.Client(
     bearer_token=bearer_token,
